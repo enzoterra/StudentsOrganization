1917938214e1c533edcdde878da138c8702a84f4
enzoterra <72806847+enzoterra@users.noreply.github.com>
2021-10-01 16:41:47 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="white">
+    <html dir="ltr">
+    <meta name="description" content="Organização dos Alunos">
+    <link rel="stylesheet" href="public/styles/style.css">
+    <!-- <link rel="icon" type="image/png" href="./public/"> -->
+    <title>StudentsOrganization</title>
+</head>
+
+<?php
+        include "src/views/pages/Home/home.php";
+?>
+</html>
---
+++ src/views/components/CreateAlunoButton/createalunobutton.php
@@ -0,0 +1,14 @@
+<style>
+    <?php
+        include "createalunobutton.css";
+    ?>
+</style>
+
+<a id="create-aluno-button" href="createaluno.php">
+    <div id="add-aluno">
+        <p>Adicionar Aluno</p>
+    </div>
+    <div id="plus-square">
+        <img id="img-plus" src="public/assets/icons/plus.svg" alt="Create aluno icon">
+    </div>
+</a>

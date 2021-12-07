8c45639fbc84c87edc06533da02631bf71fd3b65
enzoterra <enzoterra18@gmail.com>
2021-12-07 10:46:30 -0400

    Update Database.php

--- src/connection/Database.php
+++ src/connection/Database.php
@@ -3,7 +3,7 @@
         public function getConnection() {
             try {
                 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-                return new PDO('pgsql:host=localhost;dbname=web2p1', "postgres", "engipa", $options);
+                return new PDO('pgsql:host=localhost;dbname=web2p1', "postgres", "postgres", $options);
             }
             catch (PDOException $e) {
                 return null;
